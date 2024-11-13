43087b4d4af3229b8f6bc295269f8a30948a9248
Ke_Vini <67322652+Haeroou@users.noreply.github.com>
2024-11-13 21:47:13 +0900

    Update Turret_module.ino
    Update launch_missile code

--- Turret_module/Turret_module.ino
+++ Turret_module/Turret_module.ino
@@ -1,9 +1,13 @@
-
 #include <Servo.h> 
 
-Servo myservo;
+Servo myservo1;
+Servo myservo2;
 
 int pos = 90;
+
+int mainSubmotor = 9; // 방향 전환 서브모터
+int reloadSubmotor = 3; // 재장전 서브모터
+const int pirPin = 2;  // PIR 센서가 연결된 핀
 
 int trigPin_R = 5;
 int echoPin_R = 4;
@@ -19,17 +23,25 @@ long Lduration, Rduration, Lcm, Rcm;
 
 void setup() {
 
-   Serial.begin(9600);
-   pinMode(9,OUTPUT); 
-   myservo.attach(9);    
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+  pinMode(mainSubmotor,OUTPUT);
+  myservo1.attach(mainSubmotor);
+  pinMode(reloadSubmotor,OUTPUT);
+  myservo2.attach(reloadSubmotor);
 
-   myservo.write(pos);   
+  myservo1.write(pos);   
 
-   pinMode(echoPin_L, INPUT);
-   pinMode(trigPin_L, OUTPUT);
+  pinMode(echoPin_L, INPUT);
+  pinMode(trigPin_L, OUTPUT);
 
-   pinMode(echoPin_R, INPUT);
-   pinMode(trigPin_R, OUTPUT);
+  pinMode(echoPin_R, INPUT);
+  pinMode(trigPin_R, OUTPUT);
+
+  pinMode(A0, OUTPUT);              // 5번핀을 출력모드로 설정합니다.
+  pinMode(A1, OUTPUT);              // 6번핀을 출력모드로 설정합니다.
+  pinMode(A2, OUTPUT);             // 10번핀을 출력모드로 설정합니다.
+  pinMode(A3, OUTPUT);           // 11번핀을 출력모드로 설정합니다.
 
 }
 
@@ -50,26 +62,18 @@ void loop()
   Lduration = pulseIn(echoPin_L, HIGH); //echoPin 이 HIGH를 유지한 시간 저장
   Lcm = ((float)(340 * Lduration) / 10000) / 2;
 
+  Serial.print("Left = ");
+  Serial.print(Lcm);
+  Serial.print("cm || ");
+  Serial.print("Right = ");
+  Serial.print(Rcm);
+  Serial.print("cm");
+  Serial.println();
 
-   Serial.print("Left = ");
+  follow();  //초음파 거리를 비교해서 추적
+  motion_tracking(); //동작 감지 센서
 
-   Serial.print(Lcm);
-
-   Serial.print("cm || ");
-
-   Serial.print("Right = ");
-
-   Serial.print(Rcm);
-
-   Serial.print("cm");   
-
-   Serial.println();
-
-   
-
-   follow();
-
-   delay(100);
+  delay(100);
 
 }
 
@@ -79,13 +83,13 @@ void follow(){
 
     if (Lcm <= threshold || Rcm <= threshold){
 
-        if(Lcm + 4 < Rcm){
+        if(Lcm + 3 < Rcm){
 
-            pos = pos + 4;}
+            pos = pos + 3;}
 
-        if(Rcm + 4 < Lcm){
+        if(Rcm + 3 < Lcm){
 
-            pos = pos - 4;}
+            pos = pos - 3;}
 
     }
 
@@ -101,6 +105,48 @@ void follow(){
 
     }
 
-    myservo.write(pos);
+    myservo1.write(pos);
 
+}
+
+//동작 감지 센서
+void motion_tracking() {
+  int motion = digitalRead(pirPin); //PIR 센서 감지
+  if (motion == HIGH) {  // 움직임이 감지되면
+    Serial.print("동작이 감지되었습니다.");
+    launch_missile();
+    delay(2000);
+  }
+}
+
+void dc_motorON() {
+  analogWrite(A0, 255);
+  analogWrite(A1, 0);
+  analogWrite(A2, 255);
+  analogWrite(A3, 0);
+}
+
+// 미사일 발사 코드
+void launch_missile() {
+  int reload_pos = 0;
+  dc_motorON();
+  delay(100);
+  dc_motorON();
+  delay(100);
+  dc_motorON();
+  delay(100);
+  dc_motorON();
+  reload_pos = 130;
+  myservo2.write(reload_pos);
+  delay(150);
+  dc_motorON();
+  reload_pos = 0;
+  myservo2.write(reload_pos);
+  delay(100);
+  dc_motorON();
+  delay(100);
+  analogWrite(A0, 0);
+  analogWrite(A1, 0);
+  analogWrite(A2, 0);
+  analogWrite(A3, 0);
 }
